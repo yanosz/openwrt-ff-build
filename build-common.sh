53fc20dfaf5b659c6f5b24d5b249737f0eacbe6e
yanosz <yanosz@gmx.net>
2015-04-04 20:37:39 +0200

    Added lighttpd

--- build-common.sh
+++ build-common.sh
@@ -10,6 +10,14 @@ function build {
   ln -s $base/packages/pkg_fastd/libs/ $BUILD_DIR/$OPENWRT_SDK/package/libs
   ln -s $base/packages/openwrt-community/libs/nacl/ $BUILD_DIR/$OPENWRT_SDK/package/nacl
 
+  #nginx - minimal - patch default options for minimal build
+  #ln -s $base/packages/openwrt-community/net/lighttpd/ $BUILD_DIR/$OPENWRT_SDK/package/lighttpd
+  ln -s $base/packages/openwrt-community/libs/libxml2/ $BUILD_DIR/$OPENWRT_SDK/package/libxml2
+  #ln -s $base/packages/openwrt-community/libs/sqlite3  $BUILD_DIR/$OPENWRT_SDK/package/sqlite3
+  ln -s $base/packages/openwrt-community/libs/pcre/  $BUILD_DIR/$OPENWRT_SDK/package/pcre
+  #ln -s $base/packages/openwrt-community/utils/mysql/ $BUILD_DIR/$OPENWRT_SDK/package/mysq
+  ln -s $base/packages/pkg_lighttpd/lighttpd/ $BUILD_DIR/$OPENWRT_SDK/package/lighttpd
+
   #batman-adv, batctl, alfred (incl depends)
   ln -s $base/packages/openwrt_routing/alfred $BUILD_DIR/$OPENWRT_SDK/package/alfred
   ln -s $base/packages/openwrt_routing/batctl $BUILD_DIR/$OPENWRT_SDK/package/batctl
@@ -18,11 +26,13 @@ function build {
   ln -s $base/packages/fff-config-mode/luci/ $BUILD_DIR/$OPENWRT_SDK/package/config-mode
 
   # Patch batman-adv
-  #cp -a $base/patches/batman-adv/* $BUILD_DIR/$OPENWRT_SDK/package/batman-adv/patches
+  cp -a $base/patches/batman-adv/* $BUILD_DIR/$OPENWRT_SDK/package/batman-adv/patches
 
   # Bird
   ln -s $base/packages/openwrt_routing/bird $BUILD_DIR/$OPENWRT_SDK/package/bird
   ln -s $base/packages/openwrt_routing/bird-openwrt $BUILD_DIR/$OPENWRT_SDK/package/bird-openwrt
+  
+  #bmx6
   ln -s $base/packages/openwrt_routing/bmx6 $BUILD_DIR/$OPENWRT_SDK/package/bmx6
 
   # OLSRv2
